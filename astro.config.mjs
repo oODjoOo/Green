d6d57a7104fa66bdfeb76a1d2b93f49e36ebc453
Djo <djoricca@gmail.com>
2025-08-09 22:11:19 +0200

    final

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://oODjoOo.github.io",
+  site: "https://oODjoOo.github.io/Green/",
   base: "/Green/",
   integrations: [preact(), icon(), sitemap({
     filter: (page) =>
